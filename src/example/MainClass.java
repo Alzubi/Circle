b817d92af55c2e4d9be6ce25e868b6ffc8660a91
Qusai Alzubi <q3xapp@gmail.com>
2016-04-25 16:02:17 -0700

    some changes to the output

--- src/example/MainClass.java
+++ src/example/MainClass.java
@@ -1,26 +1,16 @@
 package example;
 
 public class MainClass {
-
 	
 	public static void main(String[] args) {
 
-	
 		Comparable circ1 = new Circle(10);
 		Comparable circ2 = new Circle(20);
 		
 		System.out.println("circ1 == circ2 " + circ1.equals(circ2));
-		System.out.println("circ1 < circ2 " + circ1.compareTo(circ2));
-		System.out.println("circ1 > circ2 " + circ2.compareTo(circ1));
-		
-		
 		System.out.println("circ1 < circ2 ? Should return -1: " + circ1.compareTo(circ2));
-		
 		System.out.println("circ2 < circ1 ? Should return 1: " + circ2.compareTo(circ1));
-		
-		
-		
-		
+		System.out.println("Test is done");
 	}
 
 }
